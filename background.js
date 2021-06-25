b233ce05b074d0fcd3d7ed2605ca98d53d5e9509
Ravenbtw <ravenbtwbtw@gmail.com>
2021-06-25 19:41:03 +0100

    Added landing page and dev easter egg.

--- background.js
+++ background.js
@@ -1,7 +1,14 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.create({
+      url: 'https://raven.fo/tcd-thanks',
+      active: true
+  });
+});
+
 chrome.runtime.onMessage.addListener((request) => {
     chrome.downloads.download({
       url: request.file,
       filename: request.name
     });
   }
-)
+);
